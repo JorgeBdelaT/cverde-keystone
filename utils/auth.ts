d0e8cb4fe8767577a55a94a6e98685d7591c1fb9
jorge <jabecerra@uc.cl>
2021-06-13 11:05:27 -0400

    refactor: auth helpers  function

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,2 +1,25 @@
-export const isAdmin = (user: { isAdmin: boolean }) => !!user?.isAdmin;
-export const isLoggedIn = (user: { isAdmin: boolean }) => !!user;
+import { KeystoneContext, MaybePromise } from "@keystone-next/types";
+
+export const isAdmin: (context: KeystoneContext) => MaybePromise<boolean> =
+  async (context) => {
+    const { db, session } = context;
+    if (session?.itemId) {
+      const user = await db.lists.User.findOne({
+        where: { id: session.itemId },
+      });
+
+      return !!user?.isAdmin;
+    }
+  };
+
+export const isLoggedIn: (context: KeystoneContext) => MaybePromise<boolean> =
+  async (context) => {
+    const { db, session } = context;
+    if (session?.itemId) {
+      const user = await db.lists.User.findOne({
+        where: { id: session.itemId },
+      });
+
+      return !!user;
+    }
+  };
