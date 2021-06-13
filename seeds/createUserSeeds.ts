848737c9304f17b6bef881a85e327415374c4d71
jorge <jabecerra@uc.cl>
2021-06-13 11:34:18 -0400

    feat: add users seeds

---
+++ seeds/createUserSeeds.ts
@@ -0,0 +1,35 @@
+import { KeystoneDbAPI } from "@keystone-next/types";
+
+const users = [
+  {
+    data: {
+      name: "jorge",
+      email: "jorge@cverde.org",
+      isAdmin: true,
+      password: "12345678",
+    },
+  },
+  {
+    data: {
+      name: "seba",
+      email: "seba@cverde.org",
+      isAdmin: true,
+      password: "12345678",
+    },
+  },
+  {
+    data: {
+      name: "javi",
+      email: "javi@cverde.org",
+      isAdmin: true,
+      password: "12345678",
+    },
+  },
+];
+
+export const createUserSeeds = async (db: { lists: KeystoneDbAPI<any> }) => {
+  const elementsCount = await db.lists.User.count();
+  if (elementsCount < users.length) {
+    await db.lists.User.createMany({ data: users });
+  }
+};
--- seeds/index.ts
+++ seeds/index.ts
@@ -1,6 +1,7 @@
 import { KeystoneDbAPI } from "@keystone-next/types";
 import { createSocialNetworkSeeds } from "./createSocialNetworkSeeds";
+import { createUserSeeds } from "./createUserSeeds";
 
 export const runSeeds = async (db: { lists: KeystoneDbAPI<any> }) => {
-  await Promise.all([createSocialNetworkSeeds(db)]);
+  await Promise.all([createSocialNetworkSeeds(db), createUserSeeds(db)]);
 };
